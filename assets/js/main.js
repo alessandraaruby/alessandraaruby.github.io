41fed3a9d6358962b5a8dd3f9b6ab824d23c73f4
Alessandra Aruby <39358368+alessandraaruby@users.noreply.github.com>
2018-06-08 06:37:46 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
     $routeProvider
     .when("/", {
         templateUrl : "html/pages/main.html",
