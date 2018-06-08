b544faed5635b70f4c2c2c536905187245619753
Alessandra Aruby <39358368+alessandraaruby@users.noreply.github.com>
2018-06-08 14:53:54 +0700

    Update config.js

--- blog/api/config/config.js
+++ blog/api/config/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.blogName = 'Alessandra Blog';
-config.url = 'https://alessandraaruby.github.io/';
+config.url = 'https://alessandraaruby.github.io/blog/app/';
 
 config.rss = {
   title:        'My RSS Feed',
