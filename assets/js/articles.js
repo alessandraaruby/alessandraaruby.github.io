73270c8a98585fc13146f31b881d8dbeb67457a8
Alessandra Aruby <39358368+alessandraaruby@users.noreply.github.com>
2018-10-02 19:01:51 +0700

    Update articles.js

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -4,7 +4,7 @@ var article = [
     "id": "fake",
     "date": "May 24, 2018",
     "author": "Alessandra",
-    "title": "Facebook Catfishers",
+    "title": "Facebook Catfisher",
     "desc": ["Amirah is an Asian come from the poor country Philippine who created many fake Facebook accounts...."],
     "img": "/html/contents/facebook-catfishers/image.jpg",
     "link": "https://alessandraaruby.github.io/#!fbscams"
